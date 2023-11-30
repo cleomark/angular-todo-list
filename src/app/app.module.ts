d0104ce2220f4ade66671260273b1f8da75b2d6c
cleomark <cleocalitisin@gmail.com>
2023-11-30 21:49:09 +0800

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent {
+  @Input() task: string = '';
+  @Output() removeTask = new EventEmitter<string>();
+
+  taskComplete: boolean = false;
+
+  remove() {
+    this.removeTask.emit(this.task);
+  }
+}
